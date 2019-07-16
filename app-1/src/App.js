f337a8dcad343456e207476beda9f352ddad9d3f
Brittani Excell <Brittani.Excell@yahoo.com>
2019-07-16 09:41:22 -0500

    final commit

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super(); 
+  
+    this.state = {
+      word: 'hello world'
+    }
+
+  }
+
+  handleChange(dood){
+    this.setState({ 
+      word: dood
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+        <input onChange={(blah) => this.handleChange( blah.target.value) } /> 
+      
+        { this.state.word }
+       
       </div>
     );
   }
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+  constructor() {
+    super();
+
+      this.state = {
+        food: ['pizza', 'spaghetti', 'fettucini', 'garlic rolls']
+        };  
+      }
+      handleChange(filter) {
+        this.setState({filterString: filter });
+      }
+
+      render () {
+        let foodsToDisplay = this.state.foods
+        .filter((element, index) => {
+          return element.includes(this.state.filterString);
+        })
+        .map((element, index) => {
+          return <h2 key={index}>{element}</h2>;
+        });
+        return(
+          <div className="App">
+          <input onChange={e => this.handleChange(e.target.value)} type="text" />
+          {foodsToDisplay}
+          </div>
+        );
+      }
+    }
+
+        
+       
 
 export default App;
